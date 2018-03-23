82a1c7b04d8aaff9a09e4f85d8f07d5511e0c3e4
Luis Romero <diceluisco@gmail.com>
2018-03-23 18:10:17 -0500

    add close alert box & styles and message to form

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const alert = document.getElementById('alert');
+const alertClose = document.getElementById('alertClose');
+const searchUser = document.getElementById('searchUser');
+const msgTextarea = document.getElementById('msgTextarea');
+const btnForm = document.getElementById('btnForm');
+const msgAlert = document.getElementById('msgAlert');
+
+/* add close functionality to alert box
+	 ========================================================================== */
+
+alertClose.addEventListener('click', () => {
+	alert.remove();
+});
+
+/* add styles and message to form
+	 ========================================================================== */
+
+function checkField(node) {
+	if (node.value == '') {
+		node.classList.add('invalid');
+
+	} else {
+		if (node.classList.contains('invalid')) {
+		node.classList.remove('invalid');
+		}
+	}
+}
+
+function checkForm() {
+	msgAlert.classList.remove('hide');
+
+	if (searchUser.value && msgTextarea.value) {
+		if (msgAlert.classList.contains('error')) {
+			msgAlert.classList.remove('error');
+		}
+
+		msgAlert.textContent = 'message sent';
+		msgAlert.classList.add('confirmation');
+
+	} else {
+		if (msgAlert.classList.contains('confirmation')) {
+			msgAlert.classList.remove('confirmation');
+		}
+
+		msgAlert.textContent = 'user not found or empty message';
+		msgAlert.classList.add('error');
+	}
+}
+
+btnForm.addEventListener('click', (event) => {
+	event.preventDefault();
+
+	checkField(searchUser);
+	checkField(msgTextarea);
+	checkForm();
+});
